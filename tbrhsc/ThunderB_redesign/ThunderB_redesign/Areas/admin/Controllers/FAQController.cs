aac0252b5eeb267d58fdfd9a4936c05477dddd79
Diana Sevillano <sevillano.diana@hotmail.com>
2015-03-18 00:06:13 -0400

    faq admin

--- tbrhsc/ThunderB_redesign/ThunderB_redesign/Areas/admin/Controllers/FAQController.cs
+++ tbrhsc/ThunderB_redesign/ThunderB_redesign/Areas/admin/Controllers/FAQController.cs
@@ -3,19 +3,110 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using ThunderB_redesign.Areas.admin.Models;
 using ThunderB_redesign.Models;
 
 namespace ThunderB_redesign.Areas.admin.Controllers
 {
-    public class FaqController : Controller
+    public class FaqAdmincontroller : Controller
     {
+        FaqClass objFaq = new FaqClass(); 
+
 
         public ActionResult Index()
+        {
+            var faq = objFaq.GetFaqs();
+            return View(faq);
+        }
+
+
+        // GET: /Admin/Create
+
+        public ActionResult Create() //returns the view to insert  new 
         {
             return View();
         }
 
+        // 
+        // POST: /Admin/Create
+
+        [HttpPost] //filter this action so that it only does post requests
+        public ActionResult Create(Faq faq) //takes an instance of the Faq class 
+        {
+            if (ModelState.IsValid) //if form is valid then insert concert instance
+            {
+                try
+                {
+                    objFaq.commitInsert(faq);
+                    return RedirectToAction("Index"); //after insert return to the index view
+                }
+                catch
+                {
+                    return View();
+                }
+            }
+            return View();
+        }
+
+        //
+        // GET: /Admin/Edit/
+
+        public ActionResult Edit(int id)
+        {
+            var faq = objFaq.getFaqByID(id); 
+            if (faq == null)
+            {
+                return View("NotFound");
+            }
+            return View(faq);
+        }
+
+        //
+        // POST: /Admin/Edit/
+
+        [HttpPost]
+        public ActionResult Edit(int id, Faq faq) //edit method that takes id and Faq class as parameters
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    objFaq.commitUpdate(id, faq.question, faq.answer, faq.category); //updating row with the new fields from the form 
+
+                    return RedirectToAction("Index");
+                }
+                catch
+                {
+                    return View();
+                }
+            }
+            return View();
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var faq = objFaq.getFaqByID(id); //selecting a row of the table based on the id
+            if (faq == null)
+            {
+                return View("NotFound");
+            }
+            return View(faq);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int id, Faq faq)
+        {
+            try
+            {
+                objFaq.commitDelete(id); //deleting instance
+
+                return RedirectToAction("Index"); //and returning to index view
+            }
+            catch
+            {
+                return View();
+            }
+        }
 
     }
 }
