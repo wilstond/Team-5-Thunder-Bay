383292f8e8ba381036d0e82ae2cd7b8a9a426b82
craigprofessional <craigveenstra@gmail.com>
2015-03-25 16:06:28 -0400

    Alert Feature Integration Cookies and Milk

---
+++ tbrhsc/ThunderB_redesign/ThunderB_redesign/Areas/admin/Controllers/AlertController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using ThunderB_redesign.Areas.admin.Models;
+
+namespace ThunderB_redesign.Areas.admin.Controllers
+{
+    public class AlertController : Controller
+    {
+        // GET: admin/Alert
+        AlertModelClass objAlert = new AlertModelClass();
+        public ActionResult Index()
+        {
+            var alerts = objAlert.getAlerts();
+            return View(alerts);
+        }
+    }
+
+
+}
---
+++ tbrhsc/ThunderB_redesign/ThunderB_redesign/Areas/admin/Models/AlertModelClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ThunderB_redesign.Areas.admin.Models
+{
+    public class AlertModelClass
+    {
+        //a class of the database, and an instance of that class
+        LinqDataContext objAlert = new LinqDataContext();
+        public IQueryable<AlertTable> getAlerts()
+        {
+            //entered the table and assign it to a variable
+            //grabbing all the alerts 
+            //selecting all alerts and their rows
+            var alerts = objAlert.AlertTables.Select(x => x);
+            return alerts;
+        }
+    }
+}
