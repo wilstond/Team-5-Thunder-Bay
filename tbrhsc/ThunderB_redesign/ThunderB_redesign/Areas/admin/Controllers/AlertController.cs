88a74e27102701c5906706c8f37e3cb1eebae925
craigprofessional <craigveenstra@gmail.com>
2015-03-25 16:33:54 -0400

    Alert Feature is ready to be continued

--- tbrhsc/ThunderB_redesign/ThunderB_redesign/Areas/admin/Controllers/AlertController.cs
+++ tbrhsc/ThunderB_redesign/ThunderB_redesign/Areas/admin/Controllers/AlertController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-using ThunderB_redesign.Areas.admin.Models;
+using ThunderB_redesign.Models;
 
 namespace ThunderB_redesign.Areas.admin.Controllers
 {
