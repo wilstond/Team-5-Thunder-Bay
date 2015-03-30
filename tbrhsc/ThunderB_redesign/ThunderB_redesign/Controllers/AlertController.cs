6bf99dbd5bf2b9f796e239d72cac2cf480f88b34
craigprofessional <craigveenstra@gmail.com>
2015-03-30 16:24:08 -0400

    Alert Table Mod

---
+++ tbrhsc/ThunderB_redesign/ThunderB_redesign/Controllers/AlertController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using ThunderB_redesign.Models;
+
+namespace ThunderB_redesign.Controllers
+{
+    public class AlertController : Controller
+    {
+        // GET: Alert
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
