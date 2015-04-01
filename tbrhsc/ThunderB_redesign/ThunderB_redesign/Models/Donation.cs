ac15e8d9eed6747948b5d63ea0fab6439c3c55f7
wilstond <mail2wilston@gmail.com>
2015-04-01 15:08:40 -0400

    Donations Changes - Incomplete

---
+++ tbrhsc/ThunderB_redesign/ThunderB_redesign/Models/Donation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ThunderB_redesign.Models
+{
+    public class Donation
+    {
+
+        LinqDataContext objFeedback = new LinqDataContext();
+
+
+
+    }
+}
---
+++ tbrhsc/ThunderB_redesign/ThunderB_redesign/Models/DonationVM.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace ThunderB_redesign.Models
+{
+    
+    public class DonationVM
+    {
+        public int dnr_id {get; set; }
+
+        [DisplayName("Donor Email")]
+        [Required(ErrorMessage = "Please enter your email")]
+        public string dnr_email { get; set; }
+        
+        [DisplayName("Donor Name")]
+        [Required(ErrorMessage = "Please enter your name")]
+        public string dnr_name { get; set; }
+
+        public System.Nullable<char> _dnr_anonymous { get; set; }
+
+        public int dtn_id {get; set;}
+        public int dtn_dnr_id {get; set;}
+        [DisplayName("Donor Name")]
+        [Required(ErrorMessage = "Please enter the amount")]
+        public decimal dtn_amount { get; set; }
+
+        public 
+
+    }
+
+}
