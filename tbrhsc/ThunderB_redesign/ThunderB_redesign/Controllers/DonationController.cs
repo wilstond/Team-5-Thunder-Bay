1273da9e7f016b43682d1dfe25a68ea350767198
wilstond <mail2wilston@gmail.com>
2015-04-02 00:18:46 -0400

    Donation Feature - Almost complete

--- tbrhsc/ThunderB_redesign/ThunderB_redesign/Controllers/DonationController.cs
+++ tbrhsc/ThunderB_redesign/ThunderB_redesign/Controllers/DonationController.cs
@@ -2,8 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Helpers;
 using System.Web.Mvc;
-
 using ThunderB_redesign.Models;
 
 namespace ThunderB_redesign.Controllers
@@ -23,6 +23,28 @@ namespace ThunderB_redesign.Controllers
                 ViewData["SubMenuItems for " + menuItem.menu_id.ToString()] = menuObj.getSubMenuItemsByParentId(menuItem.menu_id);
             }
 
+
+            //Code to generate monthly graph
+            var objDonationVM = new DonationVM();
+            var allDonations = objDonationVM.getAllDonationDetails();
+            var xValues = new[] { "January", "February", "March", "April", "May", "June", "July", "August", "September", "Ocotober", "November", "December" };
+            var amounts = new[] { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 };
+            foreach (var dtn in allDonations)
+            {
+                amounts[dtn.dtn_date.Month - 1] = amounts[dtn.dtn_date.Month - 1] + (double)dtn.dtn_amount;
+            }
+
+            List<string> yValues = new List<string>();
+
+            foreach (var amt in amounts)
+            {
+                yValues.Add(amt.ToString());
+            }
+
+            ViewData["xValues"] = xValues;
+            ViewData["yValues"] = yValues;
+
+
         }
 
         public ActionResult Index()
@@ -30,6 +52,64 @@ namespace ThunderB_redesign.Controllers
             return View();
         }
 
-        
+        [HttpPost]
+        public ActionResult Index(DonationVM objDonationVM)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    objDonationVM.addDonationInfo();
+                    proceedToPaypal(objDonationVM.dtn_amount);
+                    return RedirectToAction("Index");
+                }
+                catch
+                {
+                    return View();
+                }
+            }
+
+            return View();
+
+        }
+
+        public ActionResult DonationGraph()
+        {
+            var myChart = new Chart(width: 400, height: 250)
+                            .AddTitle("Monthy Report")
+                            .AddSeries(chartType: "Column",
+                            xValue: (IEnumerable<string>)@ViewData["xValues"],
+                            yValues: (IEnumerable<string>)@ViewData["yValues"])
+                            .GetBytes("jpeg");
+
+            return File(myChart, "image/jpeg");
+        }
+
+
+        //Method to process info and redirect to paypal
+
+        private void proceedToPaypal(System.Nullable<decimal> amount)
+        {
+            string url = "";
+
+            string business = "thunderbayhospital@gmail.com";  // your paypal email
+            string description = "Donation towards Thunder Bay Hospital"; // '%20' represents a space. remember HTML!
+            string country = "CA";                  // AU, US, etc.
+            string currency = "CAD";                 // AUD, USD, etc.
+            //decimal amount = 10;
+
+            url += "https://www.sandbox.paypal.com/cgi-bin/webscr" +
+                "?cmd=" + "_donations" +
+                "&business=" + business +
+                "&lc=" + country +
+                "&item_name=" + description +
+                "&amount=" + amount +
+                "&currency_code=" + currency +
+                "&bn=" + "PP%2dDonationsBF";
+
+            System.Diagnostics.Process.Start(url);
+        }
+
     }
+
 }
--- tbrhsc/ThunderB_redesign/ThunderB_redesign/Models/DonationVM.cs
+++ tbrhsc/ThunderB_redesign/ThunderB_redesign/Models/DonationVM.cs
@@ -11,6 +11,8 @@ namespace ThunderB_redesign.Models
     
     public class DonationVM
     {
+        LinqDataContext objDataContext = new LinqDataContext();
+
         public int dnr_id {get; set; }
 
         [DisplayName("Donor Email")]
@@ -25,10 +27,110 @@ namespace ThunderB_redesign.Models
 
         public int dtn_id {get; set;}
         public int dtn_dnr_id {get; set;}
-        [DisplayName("Donor Name")]
+        [DisplayName("Donor Amount")]
         [Required(ErrorMessage = "Please enter the amount")]
         public decimal dtn_amount { get; set; }
 
+        public System.DateTime dtn_date { get; set; }
+
+        public IEnumerable<donation> getAllDonations()
+        {
+            var allDonations = objDataContext.donations.Select(x => x);
+            return allDonations;
+        }
+
+        public IEnumerable<donation> getDonationsByDonorID(int id)
+        {
+            var allDonations = objDataContext.donations.Where(x => x.dtn_dnr_id == id);
+            return allDonations;
+        }
+
+        public IEnumerable<donor> getAllDonors()
+        {
+            var allDonors = objDataContext.donors.Select(x => x);
+            return allDonors;
+        }
+
+        public donor getDonorDetailsByID(int id)
+        {
+            var donorDetails = objDataContext.donors.SingleOrDefault(x => x.dnr_id == id);
+            return donorDetails;
+        }
+
+        public donor getDonorDetailsByEmail(string email)
+        {
+            var donorDetails = objDataContext.donors.SingleOrDefault(x => x.dnr_email == email);
+            return donorDetails;
+        }
+
+        public List<DonationVM> getAllDonationDetails()
+        {
+            var allDonations = getAllDonations();
+            List<DonationVM> allDonationsWithDetails = new List<DonationVM>();
+            
+            foreach(var dtn in allDonations) 
+            {
+                var donorInfo = getDonorDetailsByID(dtn.dtn_dnr_id);
+                DonationVM donationWithDonor = new DonationVM();
+                donationWithDonor.dtn_id = dtn.dtn_id;
+                donationWithDonor.dtn_dnr_id = dtn.dtn_dnr_id;
+                donationWithDonor.dtn_amount = dtn.dtn_amount;
+                donationWithDonor.dtn_date = dtn.dtn_date;
+                donationWithDonor.dnr_id = donorInfo.dnr_id;
+                donationWithDonor.dnr_name = donorInfo.dnr_name;
+                donationWithDonor.dnr_email = donorInfo.dnr_email;
+                
+                allDonationsWithDetails.Add(donationWithDonor);
+
+            }
+
+            return allDonationsWithDetails;
+
+        }
+
+        public bool addDonationInfo()
+        {
+            donor donorInfo = new donor();
+            donorInfo = getDonorDetailsByEmail(dnr_email);
+            if (donorInfo == null)
+            {
+                donorInfo = new donor();
+                donorInfo.dnr_email = dnr_email;
+                donorInfo.dnr_name = dnr_name;
+                commitInsertDonor(donorInfo);
+            }
+
+            donation donationInfo = new donation();
+            donationInfo.dtn_dnr_id = donorInfo.dnr_id;
+            donationInfo.dtn_amount = dtn_amount;
+            commitInsertDonation(donationInfo);
+
+            return true;
+        }
+
+        public bool commitInsertDonation(donation objDonation) 
+        {
+            objDataContext = new LinqDataContext();
+
+            using (objDataContext) 
+            {
+                objDataContext.donations.InsertOnSubmit(objDonation);
+                objDataContext.SubmitChanges();
+                return true;
+            }
+        }
+
+        public bool commitInsertDonor(donor objDonor)
+        {
+            objDataContext = new LinqDataContext();
+
+            using (objDataContext)
+            {
+                objDataContext.donors.InsertOnSubmit(objDonor);
+                objDataContext.SubmitChanges();
+                return true;
+            }
+        }
 
     }
 
