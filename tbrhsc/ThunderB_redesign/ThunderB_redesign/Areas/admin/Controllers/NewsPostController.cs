6bfb3af3e111aae8360a38d24ceb76fdbba8f4d4
craigprofessional <craigveenstra@gmail.com>
2015-04-07 15:14:41 -0400

    News Post Initial Creation

---
+++ tbrhsc/ThunderB_redesign/ThunderB_redesign/Areas/admin/Controllers/NewsPostController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using ThunderB_redesign.Models;
+
+namespace ThunderB_redesign.Areas.admin.Controllers
+{
+    [Authorize]
+
+    public class NewsPostController : Controller
+    {
+        // GET: admin/NewsPost
+        NewsPostAdminModel objNews = new NewsPostAdminModel();
+        public ActionResult Index()
+        {
+            //grab everthing from the news table
+            var news = objNews.getNews();
+            return View();
+        }
+    }
+}
---
+++ tbrhsc/ThunderB_redesign/ThunderB_redesign/Models/NewsPostAdminModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ThunderB_redesign.Models
+{
+    public class NewsPostAdminModel
+    {
+        //an instance of the class database
+        LinqDataContext objNews = new LinqDataContext();
+        public IQueryable<newsTable> getNews()
+        {
+            //entering the news table and assigning it into a variable
+            //grab all the news
+            var news = objNews.newsTables.Select(x => x);
+            return news;
+        }
+    }
+}
