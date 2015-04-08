38f1d0e9ef95b3de2175c5b17cce3a7b94a6d1d7
wilstond <mail2wilston@gmail.com>
2015-04-08 11:20:23 -0400

    Deleted tymce + Added Subscriber files

---
+++ tbrhsc/ThunderB_redesign/ThunderB_redesign/Models/SubscriberClass.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ThunderB_redesign.Models
+{
+    public class SubscriberClass
+    {
+
+        LinqDataContext objLinq = new LinqDataContext();
+
+        public IEnumerable<subscriber> getSubscribers()
+        {
+            var allSubscribers = objLinq.subscribers.Select(x => x);
+            return allSubscribers;
+        }
+
+        public bool commitInsert(subscriber sub) 
+        {
+            using (objLinq) 
+            {
+                objLinq.subscribers.InsertOnSubmit(sub);
+                objLinq.SubmitChanges();
+                return true;
+            }
+        }
+
+    }
+}
---
+++ tbrhsc/ThunderB_redesign/ThunderB_redesign/Models/SubscriberModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace ThunderB_redesign.Models
+{   
+    [MetadataType(typeof(SubscriberModel))]
+    public partial class subscriber
+    {
+
+    }
+
+    [Bind(Exclude = "sub_id")]
+    public class SubscriberModel
+    {
+        [DisplayName("Name")]
+        public string sub_name { get; set; }
+
+        [DisplayName("Email")]
+        [Required(ErrorMessage="Please enter your email")]
+        public string sub_email { get; set; }
+
+    }
+
+}
