aabf0057301a26cb5fad730d344bb471529628d4
craigprofessional <craigveenstra@gmail.com>
2015-04-12 15:12:35 -0400

    News Public

--- tbrhsc/ThunderB_redesign/ThunderB_redesign/Models/NewsPostPublic.cs
+++ tbrhsc/ThunderB_redesign/ThunderB_redesign/Models/NewsPostPublic.cs
@@ -1,26 +1,26 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Web;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
 
-//namespace ThunderB_redesign.Models
-//{
-//    public class NewsPostPublic
-//    {
-//        //an instance of the linq class
-//        LinqDataContext objNews = new LinqDataContext();
+namespace ThunderB_redesign.Models
+{
+    public class NewsPostPublic
+    {
+        //an instance of the linq class
+        LinqDataContext objNews = new LinqDataContext();
 
-//        public IQueryable<newsTable> getNews()
-//        {
-//            //an anonymous variable that is the instance of the linq object
-//            var allNews = objNews.newsTables.Select(x => x);
-//            return allNews;
-//        }
+        public IQueryable<newsTable> getNews()
+        {
+            //an anonymous variable that is the instance of the linq object
+            var allNews = objNews.newsTables.Select(x => x);
+            return allNews;
+        }
 
-//        //public newsTable getNewsByID(int _id)
-//        //{
-//        //    var allNew = objNews.newsTables.SingleOrDefault(x => x.Id == _id);
-//        //    return allNew;
-//        //}
-//    }
-//}
+        //public newsTable getNewsByID(int _id)
+        //{
+        //    var allNew = objNews.newsTables.SingleOrDefault(x => x.Id == _id);
+        //    return allNew;
+        //}
+    }
+}
