7cef98542f66dd882608c8347ec346ff35b83703
craigprofessional <craigveenstra@gmail.com>
2015-04-14 11:58:12 -0400

    Validation for News

--- tbrhsc/ThunderB_redesign/ThunderB_redesign/Models/NewsPostAdminModel.cs
+++ tbrhsc/ThunderB_redesign/ThunderB_redesign/Models/NewsPostAdminModel.cs
@@ -19,17 +19,20 @@ namespace ThunderB_redesign.Models
     public partial class newsValidation
     {
         [DisplayName("News Story")]
-        [Required]
+        [Required(ErrorMessage="Story Required")]
         public string stories { get; set; }
 
         [DisplayName("News Headline")]
-        [Required]
+        [Required(ErrorMessage="Headline Required")]
         public string headline { get; set; }
 
         [DisplayName("Date")]
+        [Required(ErrorMessage="Date Required")]
+        [RegularExpression(@"^((0?[13578]|10|12)(-|\/)(([1-9])|(0[1-9])|([12])([0-9]?)|(3[01]?))(-|\/)((19)([2-9])(\d{1})|(20)([01])(\d{1})|([8901])(\d{1}))|(0?[2469]|11)(-|\/)(([1-9])|(0[1-9])|([12])([0-9]?)|(3[0]?))(-|\/)((19)([2-9])(\d{1})|(20)([01])(\d{1})|([8901])(\d{1})))$", ErrorMessage = "mm/dd/yyy Format Required")]
         public string date { get; set; }
 
         [DisplayName("Author")]
+        [Required(ErrorMessage="Required Author")]
         public string author { get; set; }
     }
 }
