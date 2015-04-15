7733df5efb5f8d8788f69e619aeca7a96f1d5b6e
wilstond <mail2wilston@gmail.com>
2015-04-15 03:18:02 -0400

    Donation feature - Admin complete - except styling

--- tbrhsc/ThunderB_redesign/ThunderB_redesign/Areas/admin/Controllers/DonationAdminController.cs
+++ tbrhsc/ThunderB_redesign/ThunderB_redesign/Areas/admin/Controllers/DonationAdminController.cs
@@ -16,22 +16,52 @@ namespace ThunderB_redesign.Areas.admin.Controllers
             //Code to generate monthly graph
             var objDonationVM = new DonationVM();
             var allDonations = objDonationVM.getAllDonationDetails();
-            var xValues = new[] { "January", "February", "March", "April", "May", "June", "July", "August", "September", "Ocotober", "November", "December" };
+            
+            //X Axis values
+            List<string> xValuesPie = new List<string>();
+            List<string> xValuesLine = new List<string>();
+            var xValuesArr = new[] { "January", "February", "March", "April", "May", "June", "July", "August", "September", "Ocotober", "November", "December" };
+            //Adding to List<string>
+            foreach(var mon in xValuesArr) 
+            {
+                xValuesPie.Add(mon);
+                xValuesLine.Add(mon);
+            }
+
+            //Y Axis values
             var amounts = new[] { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 };
+            //Calculating donations for each month
             foreach (var dtn in allDonations)
             {
                 amounts[dtn.dtn_date.Month - 1] = amounts[dtn.dtn_date.Month - 1] + (double)dtn.dtn_amount;
             }
-
-            List<string> yValues = new List<string>();
-
+            //Adding to a list
+            List<string> yValuesPie = new List<string>();
+            List<string> yValuesLine = new List<string>();
             foreach (var amt in amounts)
             {
-                yValues.Add(amt.ToString());
+                yValuesPie.Add(amt.ToString());
+                yValuesLine.Add(amt.ToString());
             }
 
-            ViewData["xValues"] = xValues;
-            ViewData["yValues"] = yValues;
+
+            //Add to viewdata for line graph
+            ViewData["xValuesLine"] = xValuesLine;
+            ViewData["yValuesLine"] = yValuesLine;
+            
+            //Remove months with no donations in order to render a proper pie chart
+            for (int i = 11; i >= 0; i--)
+            {
+                if (yValuesPie.ElementAt(i).Equals("0"))
+                {
+                    xValuesPie.RemoveAt(i);
+                    yValuesPie.RemoveAt(i);
+                }
+            }
+
+            //Add to viewdata for pie chart
+            ViewData["xValuesPie"] = xValuesPie;
+            ViewData["yValuesPie"] = yValuesPie;
         }
 
         // GET: admin/DonationAdmin
@@ -96,13 +126,25 @@ namespace ThunderB_redesign.Areas.admin.Controllers
             return View();
         }
 
-        public ActionResult DonationGraph()
+        public ActionResult DonationGraphPie()
         {
-            var myChart = new Chart(width: 600, height: 400)
-                            .AddTitle("Monthy Report")
-                            .AddSeries(chartType: "Pie",
-                            xValue: (IEnumerable<string>)@ViewData["xValues"],
-                            yValues: (IEnumerable<string>)@ViewData["yValues"])
+            var myChart = new Chart(width: 450, height: 300, theme: ChartTheme.Green)
+                            .AddTitle("2015 Monthly Comparison")
+                            .AddSeries(chartType: "Doughnut",
+                            xValue: (IEnumerable<string>)@ViewData["xValuesPie"],
+                            yValues: (IEnumerable<string>)@ViewData["yValuesPie"])
+                            .GetBytes("jpeg");
+
+            return File(myChart, "image/jpeg");
+        }
+
+        public ActionResult DonationGraphLine()
+        {
+            var myChart = new Chart(width: 450, height: 300, theme: ChartTheme.Green)
+                            .AddTitle("Monthly Donation Graph")
+                            .AddSeries(chartType: "Column",
+                            xValue: (IEnumerable<string>)@ViewData["xValuesLine"],
+                            yValues: (IEnumerable<string>)@ViewData["yValuesLine"])
                             .GetBytes("jpeg");
 
             return File(myChart, "image/jpeg");
