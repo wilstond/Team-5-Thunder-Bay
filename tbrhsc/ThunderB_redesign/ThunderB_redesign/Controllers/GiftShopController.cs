c44c2e0b488ba86b1f3e2cd2428f2bca944d660a
wilstond <mail2wilston@gmail.com>
2015-04-15 13:23:19 -0400

    Updated the Linq file for Gift Shop

---
+++ tbrhsc/ThunderB_redesign/ThunderB_redesign/Controllers/GiftShopController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ThunderB_redesign.Controllers
+{
+    public class GiftShopController : Controller
+    {
+        // GET: GiftShop
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
